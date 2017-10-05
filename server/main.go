9576b58df41e1b089a1de17201816a827973f671
Julio <julio.grillo98@gmail.com>
2017-10-04 18:50:46 -0700

    removed session package and its references. implemented path argument

--- server/main.go
+++ server/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"net/http"
-	"time"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"m0ney/data"
-	"m0ney/sessions"
-	"database/sql"
+	"strconv"
+	"strings"
 )
 
 func handleList(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +16,7 @@ func handleList(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Error getting data sets"))
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -25,125 +24,31 @@ func handleList(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleSet(w http.ResponseWriter, r *http.Request) {
-	arr := data.GetSet(1)
+	arr := strings.Split(r.URL.Path, "/")
+	if len(arr) != 3 {
+		http.Error(w, "path must be /set/{integer > 1}", http.StatusBadRequest)
+		return
+	}
 
-	byt, err := json.Marshal(arr)
+
+	i, err := strconv.ParseInt(arr[2], 10, 64)
+	if err != nil || i < 1 {
+		http.Error(w, "path must be /set/{integer > 1}", http.StatusBadRequest)
+		return
+	}
+
+	set := data.GetSet(int(i))
+
+	byt, err := json.Marshal(set)
 	if err != nil {
-		http.Error(w, "couldn't marshall response", http.StatusInternalServerError)
+		http.Error(w, "couldn't marshal response", http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(byt)
 }
 
-func handleSessionCreate(w http.ResponseWriter, r *http.Request) {
-	//post request creates session
-	if r.Method != http.MethodPost {
-		http.Error(w, "send a POST request with dataset id from /list or get request to /session/{token}", http.StatusMethodNotAllowed)
-		return
-	}
-
-	byt, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "could not read body", http.StatusInternalServerError)
-		return
-	}
-
-	req := data.Set{}
-	err = json.Unmarshal(byt, &req)
-	if err != nil {
-		http.Error(w, "could not parse body", http.StatusBadRequest)
-		return
-	}
-
-	//the session request can specify and id or everything else
-	//if it only specifies the id we need to fill in everything else
-	if req.ID == 0 {
-		rows, err := data.DB.Query("SELECT `symbol`, `start`, `end`, `scale`, `table` FROM sets WHERE id = ?;", req.ID)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				http.Error(w, "specified id could not be found", http.StatusNotFound)
-			} else {
-				panic(err)
-			}
-
-			return
-		}
-		defer rows.Close()
-
-		//scan values from query and fill (replace) req with data.Set values
-		var (
-			symbol string
-			start string
-			end string
-			scale int
-			table string
-		)
-
-		rows.Scan(&symbol, &start, &end, &scale, &table)
-
-		ts, _ := time.Parse(data.SQL_TIME, start)
-		te, _ := time.Parse(data.SQL_TIME, end)
-
-		req = data.Set{
-			Symbol: symbol,
-			Start: ts,
-			End: te,
-			Scale: time.Duration(scale),
-			Table: data.Table(table),
-		}
-	}
-
-
-	//create session from request
-	token := sessions.Create(&sessions.Session{
-		SessStart: time.Now(),
-		Scale: req.Scale,
-		CurrentTime: req.Start,
-		Ticker: req.Symbol,
-	})
-
-
-	v := map[string]string{"token": token}
-
-	retByt, err := json.Marshal(v)
-	if err != nil {
-		http.Error(w, "could not marshal response", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(retByt)
-
-}
-
-func handleSession(w http.ResponseWriter, r *http.Request)  {
-
-	if r.Method != http.MethodGet {
-		http.Error(w, "must be get request", http.StatusMethodNotAllowed)
-		return
-	}
-
-	token := r.URL.Path[len("/session/"):]
-
-	sess := sessions.Get(token)
-
-	if sess == nil {
-		http.Error(w, "session " + token +" doesn't exist", http.StatusNotFound)
-		return
-	}
-
-	ret := sess.Next()
-
-	byt, err := json.Marshal(ret)
-	if err != nil {
-		http.Error(w, "could not marshal response", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(byt)
-}
 
 //entry point for server
 func main() {
@@ -154,11 +59,8 @@ func main() {
 	//list available data sets
 	m.HandleFunc("/list", handleList)
 
-	//session
-	//get (with auth) - returns values
-	//post - returns key and value for session
-	m.HandleFunc("/session", handleSessionCreate)
-	m.HandleFunc("/session/", handleSession)
+	//get the data set
+	m.HandleFunc("/set/", handleSet)
 
 
 	//start server
